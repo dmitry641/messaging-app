bafad57b392096694d5d38debe948f4f95552d29
Dmitry <dmitry6416@gmail.com>
2020-12-22 14:01:03 +0300

    messages

--- client/src/components/Conversations.js
+++ client/src/components/Conversations.js
@@ -3,13 +3,18 @@ import { ListGroup } from "react-bootstrap";
 import { useConversations } from "../contexts/ConversationsProvider";
 
 export default function Conversations() {
-  const { conversations } = useConversations();
+  const { conversations, selectConversationIndex } = useConversations();
 
   return (
     <ListGroup variant="flush">
       {conversations.map((conversation, index) => {
         return (
-          <ListGroup.Item key={index}>
+          <ListGroup.Item
+            key={index}
+            action
+            active={conversation.selected}
+            onClick={() => selectConversationIndex(index)}
+          >
             {conversation.recipients.map((r) => r.name).join(", ")}
           </ListGroup.Item>
         );
